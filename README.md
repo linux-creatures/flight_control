2f632fa32ef7eeea86cc39aaa6fa12037de57241
kiran4399 <kiran4399@gmail.com>
2016-01-24 15:23:52 +0530

    modified +  +  +

---
+++ README.md
@@ -0,0 +1,270 @@
+# Drone Pilot
+
+ Pilot software (running on companion computers) for the flight controller autopilot's. It can control and fly several flight controllers, including Pixhawk's, APM's and MultiWii's.
+
+## Current scripts:
+
+
+### MultiWii scripts:
+
+* mw-joystick.py -> Send joystick commands via UDP from a ground-station running Matlab to a FC running MultiWii software.
+
+[![Multiwii joystick (naze32)](http://img.youtube.com/vi/XyyfGp-IomE/0.jpg)](http://www.youtube.com/watch?v=XyyfGp-IomE)
+
+* mw-hover-controller.py -> Calculate commands to make a Multiwii multicopter hover over a specified x,y,z coordinate.
+
+
+### Pixhawk scripts:
+
+* pix-joystick.py -> Send joystick commands via UDP from a ground-station running Matlab to a pixhawk. Dronekit related.
+
+[![Pixhawk joystick](http://img.youtube.com/vi/TkYeQ6orN8Y/0.jpg)](http://www.youtube.com/watch?v=TkYeQ6orN8Y)
+
+* pix-logdata.py -> Script that logs data from a vehicle and a MoCap system. Dronekit related.
+
+* pix-takeoff.py -> Script that makes a pixhawk take off in the most secure way. Dronekit related.
+
+[![Example take-off](http://img.youtube.com/vi/KnjYYBKLK0s/0.jpg)](http://www.youtube.com/watch?v=KnjYYBKLK0s)
+
+* pix-goto.py -> Script that commands the vehicle to follow waypoints. 
+
+![Example goto](http://www.aldux.net/images/goto.png "SITL of this script working")
+
+* pix-velocity-vector.py -> Script that send the vehicle a velocity vector to form a square and diamond shape.
+
+![Example velocity vector](http://www.aldux.net/images/velocity.png "SITL of this script working")
+
+
+
+## Supported flight controllers:
+
+* Multiwii boards (using MSP)
+
+* Pixhawk / PX4 / APM (using mavlink & drone kit)
+
+
+
+## Supported companion computers: 
+
+* Raspberry Pi
+
+* oDroid U3 
+
+Note: Code is in python, so, any linux computer would be able tu run it.
+
+
+
+## How to:
+
+* Raspberry Pi (Rasbian)
+
+<ol start="1"><li> Update your Pi... </li></ol>
+```
+sudo apt-get update
+sudo apt-get upgrade
+```
+
+<ol start="2"><li> Next this one: </li></ol>
+```
+sudo apt-get install python-dev
+```
+
+<ol start="3"><li> More utilities: </li></ol>
+```
+sudo apt-get install screen python-wxgtk2.8 python-matplotlib python-opencv python-pip python-numpy python-serial
+```
+
+<ol start="4"><li> Then mavlink and mavproxy:</li></ol>
+```
+sudo pip install pymavlink
+sudo pip install mavproxy
+sudo pip install droneapi
+```
+
+<ol start="5"><li> Clone this repository:</li></ol>
+```
+git clone https://github.com/alduxvm/DronePilot.git
+```
+
+#### DroneAPI
+
+You have to load the module inside mavproxy.py, to do that just type:
+
+```
+module load droneapi.module.api
+
+```
+
+Now you're ready to run .py files with droneapi/dronekit! 
+
+#### Simulation
+
+If you're doing tests with simulation, don't forget to turn off the parameter checking inside the mavproxy console, so that you can arm the vehicle:
+
+```
+param set ARMING_CHECK 0
+
+```
+
+All set!! go do tests!!
+
+## Caution
+
+This code is still under heavy development, everyday I add and remove stuff. Proceed with caution.
+
+## Social networks:
+
+You can follow us in this URL's:
+
+* [Aldux.net](http://aldux.net/)
+* [Aldux.net Facebook](https://www.facebook.com/AlduxNet)
+
+## Assumptions:
+
+For the 'pix-' scripts the following assumptions must be noted:
+
+* This script assumes that the pixhawk is connected to the raspberry pi via the serial port (/dev/ttyAMA0)
+* In our setup, telemetry port 2 is configured at 115200 on the pixhawk
+* rpi camera connected (for computer vision) 
+
+## Utilities:
+
+#### ssh:
+
+/etc/ssh/ssh_config
+```
+ServerAliveInterval 5
+ClientAliveInterval 5
+```
+
+#### ad-hoc:
+
+/etc/network/interfaces
+```
+auto lo
+iface lo inet loopback
+iface eth0 inet dhcp
+ 
+auto wlan0
+iface wlan0 inet static
+  address 192.168.1.1
+  netmask 255.255.255.0
+  wireless-channel 1
+  wireless-essid RPiAdHocNetwork
+  wireless-mode ad-hoc
+```
+
+```
+sudo ifdown wlan0
+sudo ifup wlan0
+```
+
+```
+sudo apt-get install dhcp3-server
+/etc/dhcp/dhcpd.conf
+```
+
+```
+ddns-update-style interim;
+default-lease-time 600;
+max-lease-time 7200;
+authoritative;
+log-facility local7;
+subnet 192.168.1.0 netmask 255.255.255.0 {
+  range 192.168.1.5 192.168.1.150;
+}
+```
+
+In case of failure starting DHCP:
+```
+sudo service ifplugd stop
+```
+
+#### Video
+
+```
+sudo modprobe -v bcm2835-v4l2
+git clone https://github.com/mpromonet/h264_v4l2_rtspserver.git
+cd h264_v4l2_rtspserver
+cmake .
+make install
+h264_v4l2_rtspserver -H <height> -W <width> -F <fps>
+```
+
+To start:
+```
+rtsp://192.168.1.1:8554/unicast
+```
+
+#### updateDronePilot.sh 
+
+```
+#!/bin/bash
+
+echo "Deleting previous directory..."
+rm -rf DronePilot/
+echo "Done."
+echo "Cloning DronePilot repository: "
+git clone https://github.com/alduxvm/DronePilot.git
+echo "Copying mavinit.scr to proper directory..."
+mkdir DronePilot/TestQuad
+mv DronePilot/mavinit.scr DronePilot/TestQuad/
+echo "Done."
+echo "Ready to do: mavproxy.py --master=/dev/ttyAMA0 --baudrate 115200 --aircraft TestQuad"
+echo "Dont forget to go inside the DronePilot directory."
+```
+
+### Low latency video
+
+* Same DHCP configuration as before
+
+* Install "hostapd" 
+```
+sudo apt-get install hostapd netcat-traditional
+
+```
+
+* Interfaces:
+```
+auto wlan0
+iface wlan0 inet static
+  address 85.85.85.1
+  netmask 255.255.255.0
+```
+
+* /etc/hostapd/hostapd.conf 
+```
+interface=wlan0
+ssid=HDFPV
+hw_mode=g
+channel=6
+auth_algs=1
+wmm_enabled=0
+```
+
+* /etc/default/hostapd
+```
+DAEMON_CONF="/etc/hostapd/hostapd.conf"
+```
+
+* On rpi, create fifo file:
+```
+mkfifo fifo.500 
+```
+
+* On client (mac):
+```
+brew install netcat
+brew install mplayer
+```
+
+* To start (mac):
+```
+netcat -l -p 5000 | mplayer -fps 60 -cache 1024 -
+```
+
+* To start (rpi):
+```
+cat fifo.500 | nc.traditional 85.85.85.6 5000 &
+/opt/vc/bin/raspivid -o fifo.500 -t 0 -b 50000
+```
---
+++ mw-hover-controller.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+""" Drone Pilot - Control of MRUAV """
+""" mw-hover-controller.py: Script that calculates pitch and roll movements for a vehicle 
+    with MultiWii flight controller and a MoCap system in order to keep a specified position.
+"""
+
+__author__ = "Aldo Vargas"
+__copyright__ = "Copyright 2015 Aldux.net"
+
+__license__ = "GPL"
+__version__ = "1.2"
+__maintainer__ = "Aldo Vargas"
+__email__ = "alduxvm@gmail.com"
+__status__ = "Development"
+
+import time, datetime, csv, threading
+from math import *
+from modules.utils import *
+from modules.pyMultiwii import MultiWii
+import modules.UDPserver as udp
+
+# Main configuration
+logging = True
+update_rate = 0.01 # 100 hz loop cycle
+vehicle_weight = 0.84 # Kg
+u0 = 1000 # Zero throttle command
+uh = 1360 # Hover throttle command
+kt = vehicle_weight * g / (uh-u0)
+
+# MRUAV initialization
+vehicle = MultiWii("/dev/ttyUSB0")
+vehicle.getData(MultiWii.ATTITUDE)
+
+# Position coordinates [x, y, x] 
+desiredPos = {'x':0.0, 'y':0.0, 'z':1.0} # Set at the beginning (for now...)
+currentPos = {'x':0.0, 'y':0.0, 'z':0.0} # It will be updated using UDP
+
+# Initialize RC commands and pitch/roll to be sent to the MultiWii 
+rcCMD = [1500,1500,1500,1000]
+desiredRoll = 1500
+desiredPitch = 1500
+desiredThrottle = 1000
+
+# Controller PID's gains (Gains are considered the same for pitch and roll)
+p_gains = {'kp': 2.61, 'ki':0.57, 'kd':3.41, 'iMax':2, 'filter_bandwidth':50} # Position Controller gains
+h_gains = {'kp': 4.64, 'ki':1.37, 'kd':4.55, 'iMax':2, 'filter_bandwidth':50} # Height Controller gains
+
+# PID modules initialization
+rollPID =   PID(p_gains['kp'], p_gains['ki'], p_gains['kd'], p_gains['filter_bandwidth'], 0, 0, update_rate, p_gains['iMax'], -p_gains['iMax'])
+rPIDvalue = 0.0
+rollPID.setPoint(desiredPos['x'])
+pitchPID =  PID(p_gains['kp'], p_gains['ki'], p_gains['kd'], p_gains['filter_bandwidth'], 0, 0, update_rate, p_gains['iMax'], -p_gains['iMax'])
+pPIDvalue = 0.0
+pitchPID.setPoint(desiredPos['y'])
+heightPID = PID(h_gains['kp'], h_gains['ki'], h_gains['kd'], h_gains['filter_bandwidth'], 0, 0, update_rate, h_gains['iMax'], -h_gains['iMax'])
+hPIDvalue = 0.0
+heightPID.setPoint(desiredPos['z'])
+
+# Filters initialization
+f_yaw   = low_pass(20,update_rate)
+f_pitch = low_pass(20,update_rate)
+f_roll  = low_pass(20,update_rate)
+
+
+# Function to update commands and attitude to be called by a thread
+def control():
+    global vehicle, rcCMD
+    global rollPID, pitchPID, heightPID
+    global desiredPos, currentPos
+    global desiredRoll, desiredPitch, desiredThrottle
+    global rPIDvalue, pPIDvalue
+    global f_yaw, f_pitch, f_roll
+
+    while True:
+        if udp.active:
+            print "UDP server is active..."
+            break
+        else:
+            print "Waiting for UDP server to be active..."
+        time.sleep(0.5)
+
+    try:
+        if logging:
+            st = datetime.datetime.fromtimestamp(time.time()).strftime('%m_%d_%H-%M-%S')+".csv"
+            f = open("logs/mw-"+st, "w")
+            logger = csv.writer(f)
+            # V -> vehicle | P -> pilot (joystick) | M -> motion capture | C -> commanded controls
+            logger.writerow(('timestamp','Vroll','Vpitch','Vyaw','Proll','Ppitch','Pyaw','Pthrottle','x','y','z','Mroll','Mpitch','Myaw','Mode','Croll','Cpitch','Cyaw','Cthrottle'))
+        while True:
+
+            # Update joystick commands from UDP communication, order (roll, pitch, yaw, throttle)
+            rcCMD[0] = udp.message[0]
+            rcCMD[1] = udp.message[1]
+            rcCMD[2] = udp.message[2]
+            rcCMD[3] = udp.message[3]
+
+            # Coordinate map from Optitrack in the MAST Lab: X, Y, Z. NED: If going up, Z is negative. 
+            ######### WALL ########
+            #Door      | x+       |
+            #          |          |
+            #          |       y+ |
+            #---------------------|
+            # y-       |          |
+            #          |          |
+            #        x-|          |
+            #######################
+            # Update current position of the vehicle
+            currentPos['x'] = udp.message[4]
+            currentPos['y'] = udp.message[5]
+            currentPos['z'] = -udp.message[6]
+
+            # Update Attitude 
+            vehicle.getData(MultiWii.ATTITUDE)
+
+            # Filter new values before using them
+            heading = f_yaw.update(udp.message[9])
+
+            # PID updating, Roll is for Y and Pitch for X, Z is negative
+            rPIDvalue = rollPID.update(currentPos['y'])
+            pPIDvalue = pitchPID.update(currentPos['x'])
+            hPIDvalue = heightPID.update(currentPos['z'])
+            
+            # Heading must be in radians, MultiWii heading comes in degrees, optitrack in radians
+            sinYaw = sin(heading)
+            cosYaw = cos(heading)
+
+            # Conversion from desired accelerations to desired angle commands
+            desiredRoll  = toPWM(degrees( (rPIDvalue * cosYaw + pPIDvalue * sinYaw) * (1 / g) ),1)
+            desiredPitch = toPWM(degrees( (pPIDvalue * cosYaw - rPIDvalue * sinYaw) * (1 / g) ),1)
+            desiredThrottle = ((hPIDvalue + g) * vehicle_weight) / (cos(f_pitch.update(radians(vehicle.attitude['angx'])))*cos(f_roll.update(radians(vehicle.attitude['angy']))))
+            desiredThrottle = (desiredThrottle / kt) + u0
+
+            # Limit commands for safety
+            if udp.message[7] == 1:
+                rcCMD[0] = limit(desiredRoll,1000,2000)
+                rcCMD[1] = limit(desiredPitch,1000,2000)
+                rcCMD[3] = limit(desiredThrottle,1000,2000)
+                mode = 'Auto'
+            else:
+                # Prevent integrators/derivators to increase if they are not in use
+                rollPID.resetIntegrator()
+                pitchPID.resetIntegrator()
+                heightPID.resetIntegrator()
+                mode = 'Manual'
+            rcCMD = [limit(n,1000,2000) for n in rcCMD]
+
+            # Send commands to vehicle
+            vehicle.sendCMD(8,MultiWii.SET_RAW_RC,rcCMD)
+
+            row =   (time.time(), \
+                    vehicle.attitude['angx'], vehicle.attitude['angy'], vehicle.attitude['heading'], \
+                    #vehicle.rawIMU['ax'], vehicle.rawIMU['ay'], vehicle.rawIMU['az'], vehicle.rawIMU['gx'], vehicle.rawIMU['gy'], vehicle.rawIMU['gz'], \
+                    #vehicle.rcChannels['roll'], vehicle.rcChannels['pitch'], vehicle.rcChannels['throttle'], vehicle.rcChannels['yaw'], \
+                    udp.message[0], udp.message[1], udp.message[2], udp.message[3], \
+                    udp.message[4], udp.message[5], udp.message[6], \
+                    udp.message[8], udp.message[9], udp.message[10], \
+                    udp.message[7], \
+                    rcCMD[0], rcCMD[1], rcCMD[2], rcCMD[3]) 
+            if logging:
+                logger.writerow(row)
+
+            print "Mode: %s | Z: %0.3f | X: %0.3f | Y: %0.3f " % (mode, currentPos['z'], currentPos['x'], currentPos['y'])
+            # Wait time (not ideal, but its working) 
+            time.sleep(update_rate)  
+
+    except Exception,error:
+        print "Error in control thread: "+str(error)
+
+if __name__ == "__main__":
+    try:
+        logThread = threading.Thread(target=control)
+        logThread.daemon=True
+        logThread.start()
+        udp.startTwisted()
+    except Exception,error:
+        print "Error on main: "+str(error)
+        vehicle.ser.close()
+    except KeyboardInterrupt:
+        print "Keyboard Interrupt, exiting."
+        exit()
