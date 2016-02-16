0f976bcd81b0776d18de58751d33cfb9b14057f4
Kiran Kumar Lekkala <kiran@linuxcreatures.com>
2016-02-16 22:12:07 +0530

    "i" removed

--- modules/pyMultiwii.py
+++ modules/pyMultiwii.py
@@ -56,7 +56,7 @@ class MultiWii:
 
 
     """Class initialization"""
-i    def __init__(self, serPort):
+     def __init__(self, serPort):
 
         """Global variables of data"""
         self.rcChannels = {'roll':0,'pitch':0,'yaw':0,'throttle':0,'elapsed':0,'timestamp':0}
